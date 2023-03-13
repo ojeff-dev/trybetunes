121373ebcb5066fc7c71900700b02286adbf5b58
Jefferson Santos <jeff-ds@outlook.com>
2023-03-13 17:42:33 -0300

    requisito 5 - feito

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,46 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: 'Nome do Artista',
+      statusBtn: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ inputValue: target.value });
+    if (target.value.length >= 2) {
+      this.setState({ statusBtn: false });
+    } else {
+      this.setState({ statusBtn: true });
+    }
+  };
+
   render() {
+    const { statusBtn, inputValue } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Componente Search</p>
+        <form>
+          <label htmlFor="">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              placeholder={ inputValue }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="">
+            <input
+              data-testid="search-artist-button"
+              type="submit"
+              value="Pesquisar"
+              disabled={ statusBtn }
+            />
+          </label>
+        </form>
       </div>
     );
   }
