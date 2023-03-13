937aa585bfb061582bf0f2a93a6f3e8dab2d7d01
Jefferson Santos <jeff-ds@outlook.com>
2023-03-13 20:05:23 -0300

    requisito 6 - feito

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from './Header';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
-      inputValue: 'Nome do Artista',
+      inputValue: '',
       statusBtn: true,
+      loading: false,
+      loadedAlbum: false,
+      artistName: '',
+      albums: [],
+      messageAlbumNotFound: '',
     };
   }
 
@@ -19,29 +27,90 @@ class Search extends React.Component {
     }
   };
 
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({ loading: true });
+
+    const { inputValue } = this.state;
+    // add nome do artista no state
+    this.setState({ artistName: inputValue }, async () => {
+      // adiciona os álbuns no state
+      const allAlbums = await searchAlbumsAPI(inputValue);
+      allAlbums.map((album) => this.setState((prevState) => ({
+        albums: [...prevState.albums, album],
+      })));
+    });
+
+    this.setState(
+      {
+        inputValue: '',
+        loadedAlbum: true,
+        loading: false,
+        messageAlbumNotFound: 'Nenhum Álbum foi encontrado',
+      },
+    );
+  };
+
   render() {
-    const { statusBtn, inputValue } = this.state;
+    const {
+      statusBtn, inputValue, loading,
+      loadedAlbum, artistName, albums,
+      messageAlbumNotFound,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <label htmlFor="">
-            <input
-              data-testid="search-artist-input"
-              type="text"
-              placeholder={ inputValue }
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="">
-            <input
-              data-testid="search-artist-button"
-              type="submit"
-              value="Pesquisar"
-              disabled={ statusBtn }
-            />
-          </label>
-        </form>
+        {
+          loading ? <Loading />
+            : (
+              <form>
+                <label htmlFor="">
+                  <input
+                    data-testid="search-artist-input"
+                    type="text"
+                    value={ inputValue }
+                    placeholder="Nome do Artista"
+                    onChange={ this.handleChange }
+                  />
+                </label>
+                <label htmlFor="">
+                  <input
+                    data-testid="search-artist-button"
+                    type="submit"
+                    value="Pesquisar"
+                    disabled={ statusBtn }
+                    onClick={ this.handleClick }
+                  />
+                </label>
+              </form>
+            )
+        }
+        <p>{`Resultado de álbuns de: ${artistName}`}</p>
+        <section className="ContainerAlbums">
+          {
+            loadedAlbum && albums.length > 0 ? (
+              albums.map((album) => (
+                <Link
+                  className="AlbumLinks"
+                  to={ `/album/${album.collectionId}` }
+                  key={ album.collectionId }
+                  data-testid={ `link-to-album-${album.collectionId}` }
+                >
+                  <section className="AlbumCard">
+                    <img
+                      className="AlbumImage"
+                      src={ album.artworkUrl100 }
+                      alt={ album.collectionName }
+                    />
+                    <p className="AlbumInfo">{ album.collectionName }</p>
+                    <p className="AlbumInfo">{ album.artistName }</p>
+                  </section>
+                </Link>
+              ))
+            )
+              : (<span>{ messageAlbumNotFound }</span>)
+          }
+        </section>
       </div>
     );
   }
