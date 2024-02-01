f0206fa0a75072a43d625a3d4d3f1313b7378783
Jefferson Santos <jeff-ds@outlook.com>
2024-02-01 19:35:40 -0300

    (style) - updates the profile page style

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,6 +4,9 @@ import { getUser } from '../services/userAPI';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 
+import '../styles/profile.css';
+import defaultProfileIcon from '../images/default-profile-icon.png';
+
 class Profile extends React.Component {
   constructor() {
     super();
@@ -11,36 +14,94 @@ class Profile extends React.Component {
     this.state = {
       loading: true,
       infoUser: '',
+      isImageAvailable: false,
     };
   }
 
   async componentDidMount() {
     const user = await getUser();
-    this.setState({ infoUser: user, loading: false });
+    this.setState({ infoUser: user, loading: false }, () => {
+      this.checkImageAvailability(user.image);
+    });
   }
 
+  checkImageAvailability = (imageUrl) => {
+    const img = new Image();
+
+    img.onload = () => {
+      this.setState({ isImageAvailable: true });
+    };
+
+    img.onerror = () => {
+      this.setState({ isImageAvailable: false });
+    };
+
+    img.src = imageUrl;
+  };
+
   render() {
-    const { infoUser: { name, email, image, description }, loading } = this.state;
+    const {
+      infoUser: { name, email, image, description },
+      loading, isImageAvailable,
+    } = this.state;
     return (
-      <div data-testid="page-profile">
+      <div data-testid="page-profile" className="page-profile-container">
         <Header />
-        {loading ? (
-          <Loading />
-        ) : (
-          <section>
-            <img
-              data-testid="profile-image"
-              src={ image }
-              alt="Foto de perfil"
-            />
-            <p>{name}</p>
-            <p>{email}</p>
-            <p>{description}</p>
-            <div>
-              <Link to="/profile/edit">Editar perfil</Link>
-            </div>
-          </section>
-        )}
+        <section className="blue-background-profile" />
+        <section className="profile-section-container">
+          {loading && (
+            <Loading showDefaultBackground={ false } />
+          )}
+
+          {!loading && (
+            <section className="profile-content-container">
+              <section className="profile-image-container">
+                <img
+                  className="profile-image"
+                  data-testid="profile-image"
+                  src={ isImageAvailable ? image : defaultProfileIcon }
+                  alt="Foto de perfil"
+                />
+              </section>
+              <section className="user-name-info-container">
+                <div className="user-name-info-box">
+                  <span className="user-name-content-subtitle">
+                    Your name
+                  </span>
+                  <span className="user-name-content-text">
+                    {name}
+                  </span>
+                </div>
+                <div className="user-name-info-box">
+                  <span className="user-name-content-subtitle">
+                    E-mail
+                  </span>
+                  <span className="user-name-content-text">
+                    {email || 'your_email@email.com'}
+                  </span>
+                </div>
+                <div className="user-name-info-box">
+                  <span className="user-name-content-subtitle">
+                    Description
+                  </span>
+                  <span className="user-name-content-text">
+                    {description || 'your description'}
+                  </span>
+                </div>
+                <div
+                  className="user-name-info-box profile-btn-container"
+                >
+                  <Link
+                    to="/profile/edit"
+                    className="profile-edit-btn"
+                  >
+                    Edit Profile
+                  </Link>
+                </div>
+              </section>
+            </section>
+          )}
+        </section>
       </div>
     );
   }
--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -5,6 +5,10 @@ import { getUser, updateUser } from '../services/userAPI';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 
+import defaultProfileIcon from '../images/default-profile-icon.png';
+
+import '../styles/profileEdit.css';
+
 class ProfileEdit extends React.Component {
   constructor() {
     super();
@@ -13,6 +17,8 @@ class ProfileEdit extends React.Component {
       userInfo: '',
       loading: true,
       statusBtn: true,
+      isSaved: false,
+      isImageAvailable: false,
     };
   }
 
@@ -25,10 +31,11 @@ class ProfileEdit extends React.Component {
         name.length > 0
         && email.length > 0
         && description.length > 0
-        && image.length > 0
       ) {
         this.setState({ statusBtn: false });
       }
+
+      this.checkImageAvailability(image);
     });
   }
 
@@ -43,8 +50,8 @@ class ProfileEdit extends React.Component {
         && email.length > 0
         && validator.isEmail(email)
         && description.length > 0
-        && image.length > 0
       ) {
+        this.checkImageAvailability(image);
         this.setState({ statusBtn: false });
       } else {
         this.setState({ statusBtn: true });
@@ -55,7 +62,7 @@ class ProfileEdit extends React.Component {
   handleClick = (event) => {
     event.preventDefault();
 
-    this.setState({ loading: true }, async () => {
+    this.setState({ loading: true, isSaved: true }, async () => {
       const { userInfo } = this.state;
       await updateUser(userInfo);
       const { history } = this.props;
@@ -63,67 +70,131 @@ class ProfileEdit extends React.Component {
     });
   };
 
+  checkImageAvailability = (imageUrl) => {
+    const img = new Image();
+
+    img.onload = () => {
+      this.setState({ isImageAvailable: true });
+    };
+
+    img.onerror = () => {
+      this.setState({ isImageAvailable: false });
+    };
+
+    img.src = imageUrl;
+  };
+
   render() {
     const {
       loading, userInfo, statusBtn,
+      isSaved, isImageAvailable,
     } = this.state;
 
     return (
-      <div data-testid="page-profile-edit">
+      <div
+        data-testid="page-profile-edit"
+        className="page-profile-edit-container"
+      >
         <Header />
+        <section className="blue-background-profile-edit" />
         {
-          loading ? <Loading />
+          loading ? <Loading showDefaultBackground={ false } />
             : (
-              <form className="FormProfile">
-                <label htmlFor="name">
-                  Nome do usuário
-                  <input
-                    data-testid="edit-input-name"
-                    type="text"
-                    name="name"
-                    value={ userInfo.name }
-                    onChange={ this.handleChange }
+              <section className="profile-edit-content-container">
+                <section className="profile-edit-image-container">
+                  <img
+                    className="profile-image"
+                    src={
+                      isSaved && isImageAvailable
+                        ? userInfo.image : defaultProfileIcon
+                    }
+                    alt={ `${userInfo.name} icon` }
                   />
-                </label>
-                <label htmlFor="email">
-                  Email:
-                  <input
-                    type="email"
-                    name="email"
-                    data-testid="edit-input-email"
-                    value={ userInfo.email }
-                    onChange={ this.handleChange }
-                  />
-                </label>
-                <label htmlFor="description">
-                  Descrição:
-                  <textarea
-                    name="description"
-                    data-testid="edit-input-description"
-                    value={ userInfo.description }
-                    onChange={ this.handleChange }
-                  />
-                </label>
-                <label htmlFor="image">
-                  URL Imagem:
-                  <input
-                    type="text"
-                    name="image"
-                    data-testid="edit-input-image"
-                    value={ userInfo.image }
-                    onChange={ this.handleChange }
-                  />
-                </label>
-                <label>
-                  <input
-                    type="submit"
-                    value="Salvar"
-                    data-testid="edit-button-save"
-                    disabled={ statusBtn }
-                    onClick={ this.handleClick }
-                  />
-                </label>
-              </form>
+                  <label
+                    htmlFor="image-link"
+                    className="profile-edit-label-image"
+                  >
+                    <input
+                      className="profile-edit-input-image"
+                      type="text"
+                      name="image"
+                      id="image-link"
+                      data-testid="edit-input-image"
+                      placeholder="Insert link to image"
+                      value={ userInfo.image }
+                      onChange={ this.handleChange }
+                    />
+                  </label>
+                </section>
+                <form className="profile-edit-form">
+                  <label
+                    htmlFor="name"
+                    className="profile-edit-form-label"
+                  >
+                    <span className="profile-edit-form-label-subtile">
+                      Name
+                    </span>
+                    <span className="profile-edit-form-label-note">
+                      Feel free to use your social name
+                    </span>
+                    <input
+                      data-testid="edit-input-name"
+                      className="profile-edit-form-input"
+                      type="text"
+                      name="name"
+                      placeholder="Your name"
+                      value={ userInfo.name }
+                      onChange={ this.handleChange }
+                    />
+                  </label>
+                  <label
+                    htmlFor="email"
+                    className="profile-edit-form-label"
+                  >
+                    <span className="profile-edit-form-label-subtile">
+                      Email
+                    </span>
+                    <span className="profile-edit-form-label-note">
+                      Choose your best email
+                    </span>
+                    <input
+                      className="profile-edit-form-input"
+                      type="email"
+                      name="email"
+                      placeholder="your_email@email.com"
+                      data-testid="edit-input-email"
+                      value={ userInfo.email }
+                      onChange={ this.handleChange }
+                    />
+                  </label>
+                  <label
+                    htmlFor="description"
+                    className="profile-edit-form-label"
+                  >
+                    <span className="profile-edit-form-label-subtile">
+                      Description
+                    </span>
+                    <textarea
+                      className="profile-edit-form-textarea"
+                      name="description"
+                      placeholder="About you..."
+                      data-testid="edit-input-description"
+                      value={ userInfo.description }
+                      onChange={ this.handleChange }
+                    />
+                  </label>
+                  <label className="btn-profile-edit-label">
+                    <input
+                      className="btn-profile-edit-submit"
+                      type="submit"
+                      value="Save"
+                      data-testid="edit-button-save"
+                      disabled={ statusBtn }
+                      onClick={ this.handleClick }
+                    />
+                  </label>
+                </form>
+              </section>
             )
         }
       </div>
